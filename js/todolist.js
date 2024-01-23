9dbb8acf3490df2b9d6a3bb48a167275a456c39a
dieghino26 <brotzadiego@gmail.com>
2024-01-23 18:50:45 +0100

    created addTask bar

--- js/todolist.js
+++ js/todolist.js
@@ -3,12 +3,24 @@ console.log("vue ok", Vue);
 const {createApp} = Vue; 
 const app = createApp({
     data: () => ({
-        toDoList
+        toDoList,
+        newTask: ""
     }),
     methods: {
         deleteTask(id){
             this.toDoList = this.toDoList.filter(task => id !== task.id)
+        },
+        addTask(){
+            const newTask = {
+                id: new Date().toISOString(),
+                done: false,
+                text: this.newTask
+            }
+
+            this.toDoList.push(newTask)
+            this.newTask = ""
+
         }
     }
 });
-app.mount("#root")
+app.mount("#root");
