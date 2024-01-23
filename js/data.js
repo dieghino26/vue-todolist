3ca67158cb6b8451743a8c0bb5ae2e4168b3129d
dieghino26 <brotzadiego@gmail.com>
2024-01-23 16:04:12 +0100

    added to do list info

---
+++ js/data.js
@@ -0,0 +1,7 @@
+
+const toDoList = [
+    { id: 1, done: false, text: 'Fare la spesa' },
+    { id: 2, done: true, text: 'Lavare i capelli' },
+    { id: 3, done: false, text: 'Comprare una marca da bollo' },
+    { id: 4, done: false, text: 'Aggiornare il PC' }
+]
--- js/todolist.js
+++ js/todolist.js
@@ -3,7 +3,7 @@ console.log("vue ok", Vue);
 const {createApp} = Vue; 
 const app = createApp({
     data: () => ({
-        
+        toDoList
     })
 });
 app.mount("#root")
